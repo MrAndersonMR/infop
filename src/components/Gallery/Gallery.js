4c3a7d112affd6e2c0542b673f5232e4e02f4d89
Anderson <sr.andersonmendesribeiro@gmail.com>
2021-08-02 21:53:50 -0300

    update Agosto

--- src/components/Gallery/Gallery.js
+++ src/components/Gallery/Gallery.js
@@ -21,8 +21,41 @@ function getRandomSize(min, max) {
 var allImages = [];
 
 const adList = [
-  {source: 'https://mrandersonmr.github.io/infop/images/açougueSãoJoaquim.png'},
+  {source: 'https://mrandersonmr.github.io/infop/images/drogariaDaSerra.png', source2: 'https://mrandersonmr.github.io/infop/images/wgBorracharia.png'},
+  {source: 'https://mrandersonmr.github.io/infop/images/tonhoSeguros.png'},
+  {source: 'https://mrandersonmr.github.io/infop/images/nagimaq.png'},
+  {source: 'https://mrandersonmr.github.io/infop/images/casaDeCarnesCentral.png'},
+  {source: 'https://mrandersonmr.github.io/infop/images/chaveDeOuro.png', source2: 'https://mrandersonmr.github.io/infop/images/sacolãoCentral.png'},
+  {source: 'https://mrandersonmr.github.io/infop/images/casaDasFrutas.png', source2: 'https://mrandersonmr.github.io/infop/images/casaDeCarnesSantaRita.png'},
+  {source: 'https://mrandersonmr.github.io/infop/images/mateusDiesel.png'},
+  {source: 'https://mrandersonmr.github.io/infop/images/carWashCapricho.png', source2: 'https://mrandersonmr.github.io/infop/images/estacionamentoTiradentes.png'},
+  {source: 'https://mrandersonmr.github.io/infop/images/galpãoDasBaterias.png'},
+  {source: 'https://mrandersonmr.github.io/infop/images/casaRamos.png'},
+  {source: 'https://mrandersonmr.github.io/infop/images/madereiraSãoGeraldo.png', source2: 'https://mrandersonmr.github.io/infop/images/didi.png'},
+  {source: 'https://mrandersonmr.github.io/infop/images/sosCelulares.png'},
+  {source: 'https://mrandersonmr.github.io/infop/images/goCell.png'},
+  {source: 'https://mrandersonmr.github.io/infop/images/horaExtra.png'},
+  {source: 'https://mrandersonmr.github.io/infop/images/mercadoCebolinha.png', source2: 'https://mrandersonmr.github.io/infop/images/padariaBragança.png'},
+  {source: 'https://mrandersonmr.github.io/infop/images/irmãosAmino.png', source2: 'https://mrandersonmr.github.io/infop/images/nogueiraLanches.png'},
+  {source: 'https://mrandersonmr.github.io/infop/images/padariaFamíliaMarquete.png'},
+  {source: 'https://mrandersonmr.github.io/infop/images/julianaCarmoDeSouza.png'},
+  {source: 'https://mrandersonmr.github.io/infop/images/drogariaSãoCamilo.png'},
+  {source: 'https://mrandersonmr.github.io/infop/images/psicólogaMirelleSantos.png'},
+  {source: 'https://mrandersonmr.github.io/infop/images/casaDaMadeira.png'},
+  {source: 'https://mrandersonmr.github.io/infop/images/chinegoChineguinho.png'},
+  {source: 'https://mrandersonmr.github.io/infop/images/mãosArteiras.png', source2: 'https://mrandersonmr.github.io/infop/images/lojaDaNúbia.png'},
+  {source: 'https://mrandersonmr.github.io/infop/images/mecânicaCarneirinho.png'},
+  {source: 'https://mrandersonmr.github.io/infop/images/emplacarVeículos.png', source2: 'https://mrandersonmr.github.io/infop/images/marcelosMotos.png'},
+  {source: 'https://mrandersonmr.github.io/infop/images/goldim.png'},
+  {source: 'https://mrandersonmr.github.io/infop/images/hortifrutiMercadoSamuel.png'},
   {source: 'https://mrandersonmr.github.io/infop/images/armazémDoFrangoForteFrios.png'},
+  {source: 'https://mrandersonmr.github.io/infop/images/tupinambás.png'},
+  {source: 'https://mrandersonmr.github.io/infop/images/imobiliáriaCasaLeite2.png'}
+
+
+
+
+  /*{source: 'https://mrandersonmr.github.io/infop/images/armazémDoFrangoForteFrios.png'},
   //{source: '/images/arthurPet.png', source2: '/images/autoElétrica12Volts.png'},
   //{source: '/images/autoElétrica12Volts.png'},
   {source: 'https://mrandersonmr.github.io/infop/images/bomborello.png', source2: 'https://mrandersonmr.github.io/infop/images/casaDeCarnesSantaRita.png'},
@@ -58,7 +91,7 @@ const adList = [
   {source: 'https://mrandersonmr.github.io/infop/images/tonhoSeguros.png', source2: 'https://mrandersonmr.github.io/infop/images/estacionamentoTiradentes.png'},
   {source: 'https://mrandersonmr.github.io/infop/images/tupinambás.png'},
   {source: 'https://mrandersonmr.github.io/infop/images/unipac.png'}
-  /*{source: 'https://placekitten.com/', width: getRandomSize(200, 400), height: getRandomSize(200, 400)}*/
+  {source: 'https://placekitten.com/', width: getRandomSize(200, 400), height: getRandomSize(200, 400)}*/
 ]
 
 /*export default function Gallery () {
--- src/components/Title/Title.js
+++ src/components/Title/Title.js
@@ -11,7 +11,7 @@ const Title = () => (
         OPÇÃO
       </h1>
       <h5>
-        2ª Edição <b>· Julho</b> de 2021 <virtual>versão virtual</virtual>  <br/>
+        3ª Edição <b>· Agosto</b> de 2021 <virtual>versão virtual</virtual>  <br/>
       </h5>
       <h5>
         <name>
@@ -26,10 +26,10 @@ const Title = () => (
           <b>Opção</b>
         </sub>
       </h5>
-      <h3>Julho de 20<b>21</b></h3>
+      <h3>Agosto de 20<b>21</b></h3>
       <hr size="2" width="5%" color="greenyellow" />
       <h6>
-        Informativo - Ano XIX - <b>Julho</b> de 2021 - NºCCXXVIII <br/>
+        Informativo - Ano XIX - <b>Agosto</b> de 2021 - NºCCXXIX <br/>
         São João Nepomuceno e Região - Minas Gerais <br/>
       </h6>
     </div>
